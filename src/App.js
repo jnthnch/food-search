19f0c9b7c44ef44cf8a3d18f2f5589ed109b04e9
jnthnch <jchu127@gmail.com>
2019-09-11 19:47:18 -0700

    Fix search function to only do searches with queries of 3+ characters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import FoodItem from './components/foodItem.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -27,11 +28,13 @@ class App extends React.Component {
 
   performSearch() {
     let query = this.state.searchQuery;
-    fetch(`https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${query}`)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ foods: data })
-      });
+    if (query.length > 2) {
+      fetch(`https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${query}`)
+        .then(res => res.json())
+        .then(data => {
+          this.setState({ foods: data })
+        });
+    }
   }
 
   handleChangeDelayed(event) {
@@ -48,46 +51,52 @@ class App extends React.Component {
   }
 
 
-  renderList(foods) {
-    var list = [];
-    foods.forEach(v => {
-      list.push(
-        <div style={{ marginBottom: "40px", display: "flex", flexDirection: "column" }}>
-          <div>
-            name: {v.name}
-          </div>
-          <div>
-            brand: {v.brand}
-          </div>
-          <div>
-            calories: {v.calories}
-          </div>
-          <div>
-            portion: {v.portion}
-          </div>
-        </div>
-      )
-    });
-    return (
-      <div>
-        <h3>Result:</h3>
-        {list}
-      </div>
-    )
-  }
+  // renderList(foods) {
+  //   var list = [];
+  //   foods.forEach(v => {
+  //     list.push(
+  //       <div style={{ marginBottom: "40px", display: "flex", flexDirection: "column" }}>
+  //         <div>
+  //           name: {v.name}
+  //         </div>
+  //         <div>
+  //           brand: {v.brand}
+  //         </div>
+  //         <div>
+  //           calories: {v.calories}
+  //         </div>
+  //         <div>
+  //           portion: {v.portion}
+  //         </div>
+  //       </div>
+  //     )
+  //   });
+  //   return (
+  //     <div>
+  //       <h3>Result:</h3>
+  //       {list}
+  //     </div>
+  //   )
+  // }
 
   render() {
+    let foodItems = this.state.foods.map((item) => {
+      return <FoodItem item={item}></FoodItem>
+    })
+
     return (
       <div className="App">
-        <h1>Noom Food Explorer</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Enter the key word:
-          <input type="text" value={this.state.value} onKeyUp={this.handleChangeDelayed} />
-          </label>
-          <button type="submit">Search</button>
-        </form>
-        {this.renderList(this.state.foods)}
+        <div className="header">
+          <div className="header-search-content">
+            <text className="Noom-Food-Explorer-Text">Noom Food Explorer</text>
+            <text className="search-text">Enter the key word:</text>
+            <form className="search-box" onSubmit={this.handleSubmit}>
+              <input className="input-box" type="text" value={this.state.value} onKeyUp={this.handleChangeDelayed} />
+              <button className="search-button" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+        {foodItems}
       </div>
     );
   }
