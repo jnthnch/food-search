2796eacb5b151a667546cc6e576204f0fd3aa176
jnthnch <jchu127@gmail.com>
2019-09-11 18:45:48 -0700

    Add foodItem component

---
+++ src/components/foodItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const foodItem = (props) => {
+  return (
+    <div className='food-item-box'>
+      <div>
+        name: {props.item.name}
+      </div>
+      <div>
+        brand: {props.item.brand}
+      </div>
+      <div>
+        calories: {props.item.calories}
+      </div>
+      <div>
+        portion: {props.item.portion}
+      </div>
+    </div>
+  )
+
+}
+
+export default foodItem;
