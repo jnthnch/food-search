20910b28894feae463d37c5e3e2655361fdfa8f4
jnthnch <jchu127@gmail.com>
2019-09-11 18:14:32 -0700

    Implement search function for when user has stopped typing

--- src/App.js
+++ src/App.js
@@ -4,33 +4,55 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.timeout = 0;
     this.state = {
-      searchQuery: '', 
+      searchQuery: '',
       foods: []
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeDelayed = this.handleChangeDelayed.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.performSearch = this.performSearch.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    fetch('https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=chicken')
+    let query = this.state.searchQuery
+    fetch(`https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${query}`)
       .then(res => res.json())
       .then(data => {
-        this.setState({foods: data})
+        this.setState({ foods: data })
       });
   }
 
-  handleChange(event) {
-    this.setState({searchQuery: event.target.value});
+  performSearch() {
+    let query = this.state.searchQuery;
+    fetch(`https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${query}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ foods: data })
+      });
   }
+
+  handleChangeDelayed(event) {
+    let searchText = event.target.value;
+    this.setState({
+      searchQuery: searchText
+    })
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+    this.timeout = setTimeout(() => {
+      this.performSearch();
+    }, 1000)
+  }
+
 
   renderList(foods) {
     var list = [];
     foods.forEach(v => {
       list.push(
-        <div style={{marginBottom: "40px", display: "flex", flexDirection: "column"}}>
+        <div style={{ marginBottom: "40px", display: "flex", flexDirection: "column" }}>
           <div>
             name: {v.name}
           </div>
@@ -59,10 +81,10 @@ class App extends React.Component {
       <div className="App">
         <h1>Noom Food Explorer</h1>
         <form onSubmit={this.handleSubmit}>
-        <label>
-          Enter the key word:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
+          <label>
+            Enter the key word:
+          <input type="text" value={this.state.value} onKeyUp={this.handleChangeDelayed} />
+          </label>
           <button type="submit">Search</button>
         </form>
         {this.renderList(this.state.foods)}
